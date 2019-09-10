aa34d234d6abc63e75b5a831f45ac72a1489e4ad
astee529@ <astee529@students.bju.edu>
2019-09-09 20:57:59 -0400

    basic line-following program

--- TestDrive-2019-09-09T23-49-02/src/main.cpp
+++ TestDrive-2019-09-09T23-49-02/src/main.cpp
@@ -12,7 +12,7 @@ vex::drivetrain dt(motor_left, motor_right, 319.1764, 292.1, vex::distanceUnits:
 //#endregion config_globals
 
 //value returned when staring at a line:
-int line_value = 60;
+int line_value = 71;
 //value returned when staring at the floor:
 int floor_value = 30;
 
@@ -20,15 +20,38 @@ bool sees_line(int tracker_value) {
     return abs(tracker_value - line_value) <= abs(tracker_value - floor_value);
 }
 
+bool see_white_line( int value ) {
+    return value <= 67;
+}
+
 int main(void) {
     //go slow, as this algorithm doesn't handle overshooting the line very well
     dt.setVelocity(10,velocityUnits::pct);
+    // while(true)
+    // {
+    //   Brain.Screen.print(line_tracker_left.value(pct));
+    //   Brain.Screen.newLine();
+    // }
     while(true) {
-        if(sees_line(line_tracker_right.value(pct)) || sees_line(line_tracker_left.value(pct)) )
+        // if(sees_line(line_tracker_right.value(pct)) || sees_line(line_tracker_left.value(pct)) )
+        // {
+        //     //right tracker saw line, we're drifting left
+        //     dt.setVelocity( 0, velocityUnits::pct );
+        // } 
+        if( see_white_line( line_tracker_right.value( pct ) ) )
         {
-            //right tracker saw line, we're drifting left
-            dt.setVelocity( 0, velocityUnits::pct );
-        } else {
+          dt.turn(turnType::right);
+          Brain.Screen.print( line_tracker_right.value( pct ) );
+          Brain.Screen.newLine();
+        } 
+        else if( see_white_line( line_tracker_left.value( pct ) ) )
+        {
+          dt.turn(turnType::left);
+          Brain.Screen.print( line_tracker_left.value( pct ) );
+          Brain.Screen.newLine();
+        } 
+        else 
+        {
             //neither tracker sees the line, so we're straddling it
             dt.drive(fwd);
         }
