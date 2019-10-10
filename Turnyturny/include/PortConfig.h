2709201f048753781fbeab4ce74f3648900c1415
astee529@ <astee529@students.bju.edu>
2019-10-10 13:50:24 -0400

    Created PortConfig file to hold the port assignments for all components attached to the brain

---
+++ Turnyturny/include/PortConfig.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "vex.h"
+#include "LineTracker.h"
+
+using namespace vex;
+
+// A global instance of vex::brain used for printing to the V5 brain screen
+vex::brain       Brain;
+
+// define your global instances of motors and other devices here
+// MOTORS
+vex::motor RightMotor = vex::motor( vex::PORT1, true );
+vex::motor LeftMotor = vex::motor( vex::PORT10, false );
+vex::drivetrain dt( LeftMotor, RightMotor ); 
+
+// SENSORS
+LineTracker line_tracker_left( line(Brain.ThreeWirePort.G) );
+LineTracker line_tracker_right( line(Brain.ThreeWirePort.H) );
+vex::sonar ultra(Brain.ThreeWirePort.A);
+vex::bumper bumpy( Brain.ThreeWirePort.F );
