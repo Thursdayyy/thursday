468a336ae41358b0893502d69aa977113aa08682
astee529@ <astee529@students.bju.edu>
2019-10-09 22:09:55 -0400

    added driving all the way into bin, backing out of bin, and turning out of bin

--- Turnyturny/src/main.cpp
+++ Turnyturny/src/main.cpp
@@ -6,15 +6,7 @@
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
-// Drive Forward
-// This program instructs your robot to move forward at half power for three
-// seconds. There is a two second pause at the beginning of the program.
-//
-// Robot Configuration: 
-// [Smart Port]    [Name]        [Type]           [Description]       [Reversed]
-// Motor Port 1    LeftMotor     V5 Smart Motor    Left side motor     false
-// Motor Port 10   RightMotor    V5 Smart Motor    Right side motor    true
-//
+
 #include "vex.h"
 
 using namespace vex;
@@ -25,30 +17,61 @@ vex::brain       Brain;
 // define your global instances of motors and other devices here
 vex::motor RightMotor   = vex::motor( vex::PORT1, true );
 vex::motor LeftMotor  = vex::motor( vex::PORT10, false );
-vex::line line_tracker( Brain.ThreeWirePort.A );
+vex::line line_tracker_left( Brain.ThreeWirePort.G );
+vex::line line_tracker_right( Brain.ThreeWirePort.H );
+
 vex::drivetrain dt( LeftMotor, RightMotor ); 
 vex::sonar ultra(Brain.ThreeWirePort.A);
 
-vex::bumper bumpy( Brain.ThreeWirePort.C );
+vex::bumper bumpy( Brain.ThreeWirePort.F );
 
 bool see_white_line( int value ) {
-  return value <= 67;
+  return value <= 65;
+}
+
+void println( const char* string )
+{
+  Brain.Screen.print(string);
+  Brain.Screen.newLine();
 }
 
 int main() {
-  Brain.Screen.print("TurnyTurny Program has Started.");
+  // Brain.Screen.print("TurnyTurny Program has Started.");
+  // Brain.Screen.newLine();
 
-  vex::task::sleep(3000);
+  println( "TurnyTurnt Program has Started." );
+
+  // while(true)
+  // {
+  //   Brain.Screen.print("%f", ultra.distance(distanceUnits::in));
+  //   Brain.Screen.newLine();
+  // }
+
+  vex::task::sleep(4500);
+
+  while(true)
+  {
+    if( !bumpy.pressing() )
+    {
+      dt.drive(directionType::rev);
+    }
+    else {
+      dt.stop();
+      vex::task::sleep(3000);
+      break;
+    }
+  }
 
   dt.setVelocity(20, velocityUnits::pct);
 
-  dt.driveFor(directionType::fwd, 30, distanceUnits::cm );
+  dt.driveFor(directionType::fwd, 13, distanceUnits::in );
 
   while ( true )
   {
-    Brain.Screen.print("sensor value:%d", ultra.value());
+    // Brain.Screen.print("sensor value:%d", ultra.value());
+    // Brain.Screen.newLine();
     Brain.Screen.newLine();
-    if ( see_white_line( line_tracker.value( pct ) ) )
+    if ( see_white_line( line_tracker_left.value( pct ) ) && see_white_line( line_tracker_right.value( pct ) ) )
     {
       dt.stop();
       break;
@@ -59,15 +82,29 @@ int main() {
     }
   }
 
-  dt.driveFor( directionType::fwd, 7, distanceUnits::cm );
+  dt.driveFor( directionType::rev, 3.5, distanceUnits::in );
 
   LeftMotor.spinFor( 235*2, vex::rotationUnits::deg );
 
-  vex::task::sleep(1000);
+  vex::task::sleep(500);
 
-  LeftMotor.spinFor( -235*2, vex::rotationUnits::deg );
-
-  while(1) {
-    vex::task::sleep(100);
+  double dist = ultra.distance(distanceUnits::in);
+  
+  while( ultra.distance(distanceUnits::in) > 3.4 )
+  {
+    dt.drive(directionType::fwd);
   }
+
+  dt.stop();
+  vex::task::sleep(3000);
+
+  while( ultra.distance(distanceUnits::in) < dist )
+  {
+    dt.drive(directionType::rev);
+  }
+
+  dt.stop();
+
+  LeftMotor.spinFor( -233*2, vex::rotationUnits::deg );
+
 }
