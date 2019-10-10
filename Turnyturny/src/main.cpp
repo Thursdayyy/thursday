6a6d997b8be3ecedb429afcf3ae6a15baf13e0e1
astee529@ <astee529@students.bju.edu>
2019-10-09 22:36:42 -0400

    Added a few comments and refactored method for line detection for easier calling

--- Turnyturny/src/main.cpp
+++ Turnyturny/src/main.cpp
@@ -15,23 +15,28 @@ using namespace vex;
 vex::brain       Brain;
 
 // define your global instances of motors and other devices here
-vex::motor RightMotor   = vex::motor( vex::PORT1, true );
-vex::motor LeftMotor  = vex::motor( vex::PORT10, false );
+// MOTORS
+vex::motor RightMotor = vex::motor( vex::PORT1, true );
+vex::motor LeftMotor = vex::motor( vex::PORT10, false );
+vex::drivetrain dt( LeftMotor, RightMotor ); 
+
+// SENSORS
 vex::line line_tracker_left( Brain.ThreeWirePort.G );
 vex::line line_tracker_right( Brain.ThreeWirePort.H );
-
-vex::drivetrain dt( LeftMotor, RightMotor ); 
 vex::sonar ultra(Brain.ThreeWirePort.A);
-
 vex::bumper bumpy( Brain.ThreeWirePort.F );
 
-bool see_white_line( int value ) {
-  return value <= 65;
+constexpr int LINE_ANALOG_PCT = 65;
+
+bool over_line( vex::line& line_tracker ) // TODO: Make classes for each sensor
+{
+  return line_tracker.value( analogUnits::pct ) <= LINE_ANALOG_PCT;
 }
 
-void println( const char* string )
+// Prints a formatted std::string to the VEX Brain screen
+void println( const char* c_str )
 {
-  Brain.Screen.print(string);
+  Brain.Screen.print( c_str );
   Brain.Screen.newLine();
 }
 
@@ -41,14 +46,10 @@ int main() {
 
   println( "TurnyTurnt Program has Started." );
 
-  // while(true)
-  // {
-  //   Brain.Screen.print("%f", ultra.distance(distanceUnits::in));
-  //   Brain.Screen.newLine();
-  // }
-
+  dt.setVelocity(20, velocityUnits::pct);
   vex::task::sleep(4500);
 
+  // drop off the button assembly
   while(true)
   {
     if( !bumpy.pressing() )
@@ -62,16 +63,16 @@ int main() {
     }
   }
 
-  dt.setVelocity(20, velocityUnits::pct);
-
+  // drive past starting square (for now 13 is an arbitrary value that gets us past the square. Probably need to use the line sensors to determine when we've passed the square)
   dt.driveFor(directionType::fwd, 13, distanceUnits::in );
 
+  // go down center line and search for bins
   while ( true )
   {
-    // Brain.Screen.print("sensor value:%d", ultra.value());
-    // Brain.Screen.newLine();
     Brain.Screen.newLine();
-    if ( see_white_line( line_tracker_left.value( pct ) ) && see_white_line( line_tracker_right.value( pct ) ) )
+
+    // stop at a cross-mark
+    if ( over_line( line_tracker_left ) && over_line( line_tracker_right ) )
     {
       dt.stop();
       break;
@@ -82,19 +83,20 @@ int main() {
     }
   }
 
+  // need to back up a slight bit in order for the front wheels to not collide with the bin wall when turning in
   dt.driveFor( directionType::rev, 3.5, distanceUnits::in );
 
   LeftMotor.spinFor( 235*2, vex::rotationUnits::deg );
 
   vex::task::sleep(500);
 
+  // for now using a distance marker to know how far to back out
   double dist = ultra.distance(distanceUnits::in);
   
   while( ultra.distance(distanceUnits::in) > 3.4 )
   {
     dt.drive(directionType::fwd);
   }
-
   dt.stop();
   vex::task::sleep(3000);
 
@@ -102,9 +104,7 @@ int main() {
   {
     dt.drive(directionType::rev);
   }
-
   dt.stop();
 
   LeftMotor.spinFor( -233*2, vex::rotationUnits::deg );
-
 }
